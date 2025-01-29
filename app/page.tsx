df17b0b3e3273099f2da3094de3f08d72cffa689
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2025-01-29 11:10:25 -0700

    remove unneeded code

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+/*
+<ai_context>
+This server page shows a basic home page.
+</ai_context>
+*/
+
+"use server"
+
+export default async function HomePage() {
+  return (
+    <div className="flex-1 p-4 pt-0">
+      <h1>Home Page</h1>
+    </div>
+  )
+}
