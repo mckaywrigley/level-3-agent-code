74d5e34c836c8c8742d7f7bd75e7a7b83aa40a46
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2025-01-29 19:58:45 -0700

    add playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    ["dot"], // or 'line' or something else you like
+    ["json", { outputFile: "playwright-report/report.json" }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
