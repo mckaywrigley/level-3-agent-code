aa9183318d599259bbbb330eaba45c3ea3a5a0c9
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2025-01-29 20:38:31 -0700

    change

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Link from "next/link"
 export default async function HomePage() {
   return (
     <div className="flex-1 p-4 pt-0">
-      <h1>Welcome</h1>
+      <h1>yo yo yo</h1>
 
       <Link href="/about">About</Link>
     </div>
