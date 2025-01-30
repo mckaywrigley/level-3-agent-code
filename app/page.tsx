5e26578b52142e34a88963d46de109d3b55b2d4f
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2025-01-29 17:28:53 -0700

    add about page

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,14 @@ This server page shows a basic home page.
 
 "use server"
 
+import Link from "next/link"
+
 export default async function HomePage() {
   return (
     <div className="flex-1 p-4 pt-0">
-      <h1>Home Page</h1>
+      <h1>Welcome</h1>
+
+      <Link href="/about">About</Link>
     </div>
   )
 }
