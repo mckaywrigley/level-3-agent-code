3d082a5c8f81c98516a035bcf3828e5f867a7ace
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2025-02-05 18:34:57 -0700

    test

--- __tests__/unit/AboutPage.test.tsx
+++ __tests__/unit/AboutPage.test.tsx
@@ -1,21 +1,12 @@
-import AboutPage from "@/app/about/page";
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import AboutPage from "@/app/about/page"
+import "@testing-library/jest-dom"
+import { render, screen } from "@testing-library/react"
 
 describe("AboutPage Component", () => {
   it("renders correctly and displays the About page text", async () => {
-    const content = await AboutPage();
-    render(content);
-    const headingElement = screen.getByText("About Page");
-    expect(headingElement).toBeInTheDocument();
-  });
-
-  it("optionally renders a comment section if present", async () => {
-    const content = await AboutPage();
-    render(content);
-    const commentSection = screen.queryByTestId("comment-section");
-    if (commentSection) {
-      expect(commentSection).toBeInTheDocument();
-    }
-  });
-});
+    const content = await AboutPage()
+    render(content)
+    const headingElement = screen.getByText("About Page")
+    expect(headingElement).toBeInTheDocument()
+  })
+})
